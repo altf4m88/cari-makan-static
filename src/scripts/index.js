4a0a6de890c48cb4c5465c2c99c4fbbe007d9206
altf4m88 <fadhilmauladhani@gmail.com>
2021-05-07 08:34:00 +0700

    add burger menu for small screen

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,21 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
 console.log('Hello Coders! :)');
+
+const menuLines = document.querySelector(".hamburger");
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const nav = document.querySelector(".nav-list");
+
+menuLines.addEventListener('click', function(event){
+    nav.classList.toggle('on-click');
+    event.stopPropagation();
+});
+
+hero.addEventListener('click', function () {
+    nav.classList.remove('on-click');
+});
+
+main.addEventListener('click', function () {
+    nav.classList.remove('on-click');
+});
