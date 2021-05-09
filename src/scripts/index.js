dace6656ff68f83904b25f6ebfc49aa143e4ec28
altf4m88 <fadhilmauladhani@gmail.com>
2021-05-09 13:15:27 +0700

    print html element with JS loop and delete unused file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,8 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
+const {restaurants} = require('../DATA.json');
+
 console.log('Hello Coders! :)');
 
 const menuLines = document.querySelector(".hamburger");
@@ -20,3 +22,27 @@ hero.addEventListener('click', function () {
 main.addEventListener('click', function () {
     nav.classList.remove('on-click');
 });
+
+const cardParent = document.querySelector('#cards');
+let cards = "";
+
+restaurants.forEach(data => {
+    cards += `
+        <article tabindex="0" class="card" id=${data.id}>
+            <img class="card-image" src=${data.pictureId} alt="Foto ${data.name}">
+            <div class="card-content">
+                <h2 class="card-title">${data.name}, ${data.city}</h2>
+                <p class="card-description">
+                    ${data.description}
+                </p>
+                <div class="card-rating">
+                    <p><i class="fas fa-star"></i> ${data.rating}</p>
+                </div>
+            </div>
+        </article>
+    `;
+});
+
+cardParent.innerHTML = cards;
+
+console.log(html);
